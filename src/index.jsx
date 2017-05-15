07dfb57157138e0e1a955af32961b0911cd8eb94
Alexei <alexeiatyahoodotcom@gmail.com>
2017-05-14 22:05:42 -0400

    Upgrade all dependencies.

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,7 @@
  */
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 function number_format(num, decimals) {
   if (decimals === undefined) {
@@ -24,37 +25,39 @@ var RGB_DATA = {
   2017: { one: 0, two: 2 },
 };
 
-var App = React.createClass({
-  getInitialState: function () {
-    return {
-      rateOne: null,
-      rateTwo: null,
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      rateOne: "",
+      rateTwo: "",
       rent: '2000.00',
       showDetails: false,
       useProposedRates: false,
     };
-  },
+  }
 
-  handleChange: function (event) {
+  handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value
     });
-  },
+  }
 
-  handleButton: function () {
+  handleButton() {
     this.setState({
       rent: document.getElementById('rent').value
     });
-  },
+  }
 
-  showDetails: function () {
+  showDetails(e) {
     this.setState({
       showDetails: true
     });
-    return false;
-  },
+    e.preventDefault();
+  }
 
-  useProposedRates: function () {
+  useProposedRates() {
     this.setState({
       useProposedRates: true
     });
@@ -64,9 +67,9 @@ var App = React.createClass({
         window.scroll(0, window.pageYOffset + rate_input.getBoundingClientRect().top);
       }
     }, 100);
-  },
+  }
 
-  render: function () {
+  render() {
     var header = (<div>
       <h1>NYC Rent-Stabilized Apartment<br />Lease Renewal Calculator</h1>
       If you live in a rent-stabilized apartment in New York City, and your lease is up for renewal around September 1st, this calculator can help pick the lease duration.
@@ -75,10 +78,10 @@ var App = React.createClass({
     var footer = (<div>
       <h2>Resources</h2>
       <ul>
-        <li><a href="http://streeteasy.com/talk/discussion/27124-lease-options-in-a-rent-stabilized-apt" target="_blank">Lease Options in a Rent-Stabilized Apt</a></li>
-        <li><a href="http://www.lesliebeslie.com/2012/12/17/lets-talk-about-lease-renewal-rent-stabilization/" target="_blank">Let’s Talk About Lease Renewal &amp; Rent Stabilization</a></li>
-        <li><a href="https://amirentstabilized.com/" target="_blank">Am I Rent-Stabilized?</a></li>
-        <li><a href="http://www.nycrgb.org/" target="_blank">NYC Rent Guidelines Board</a></li>
+        <li><a href="http://streeteasy.com/talk/discussion/27124-lease-options-in-a-rent-stabilized-apt" target="_blank" rel="noopener noreferrer">Lease Options in a Rent-Stabilized Apt</a></li>
+        <li><a href="http://www.lesliebeslie.com/2012/12/17/lets-talk-about-lease-renewal-rent-stabilization/" target="_blank" rel="noopener noreferrer">Let’s Talk About Lease Renewal &amp; Rent Stabilization</a></li>
+        <li><a href="https://amirentstabilized.com/" target="_blank" rel="noopener noreferrer">Am I Rent-Stabilized?</a></li>
+        <li><a href="http://www.nycrgb.org/" target="_blank" rel="noopener noreferrer">NYC Rent Guidelines Board</a></li>
       </ul>
     </div>);
 
@@ -88,7 +91,7 @@ var App = React.createClass({
       return (<div>
         {header}
         <div className="notice">
-          <p>Oops, we don't have NYC Rent Guidelines Board apartment lease renewal rates for {year}.</p>
+          <p>Oops, we don&apos;t have NYC Rent Guidelines Board apartment lease renewal rates for {year}.</p>
           <p>Looks like this app is way out of date.</p>
         </div>
         {footer}
@@ -99,7 +102,7 @@ var App = React.createClass({
       return (<div>
         {header}
         <div className="notice">
-          <p>We don't have NYC Rent Guidelines Board apartment lease renewal rates for {year+1}. You could check their <a href="http://www.nycrgb.org/html/guidelines/apt.html" target="_blank">website</a> for proposed rates.</p>
+          <p>We don&apos;t have NYC Rent Guidelines Board apartment lease renewal rates for {year+1}. You could check their <a href="http://www.nycrgb.org/html/guidelines/apt.html" target="_blank" rel="noopener noreferrer">website</a> for proposed rates.</p>
           <p>
             <label htmlFor="rateOne">One-year renewal lease adjustment for {year + 1}:</label>
             <input
@@ -110,7 +113,7 @@ var App = React.createClass({
               id="rateOne"
               name="rateOne"
               value={this.state.rateOne}
-              onChange={this.handleChange}
+              onChange={this.handleChange.bind(this)}
               autoComplete="off" />%
           </p>
           <p>
@@ -123,7 +126,7 @@ var App = React.createClass({
               id="rateTwo"
               name="rateTwo"
               value={this.state.rateTwo}
-              onChange={this.handleChange}
+              onChange={this.handleChange.bind(this)}
               autoComplete="off" />%
           </p>
           <p>
@@ -131,7 +134,7 @@ var App = React.createClass({
               type="button"
               id="use-proposed-rates-button"
               value="Use proposed rates"
-              onClick={this.useProposedRates} />
+              onClick={this.useProposedRates.bind(this)} />
           </p>
         </div>
         {footer}
@@ -171,14 +174,14 @@ var App = React.createClass({
           id="rent"
           name="rent"
           value={this.state.rent}
-          onChange={this.handleChange}
+          onChange={this.handleChange.bind(this)}
           autoComplete="off" /> / mo
 
         <input
           type="button"
           id="update-button"
           value="Update"
-          onClick={this.handleButton}
+          onClick={this.handleButton.bind(this)}
           style={{
             float: "right"
           }}/>
@@ -189,14 +192,18 @@ var App = React.createClass({
 
     var summary;
     if (totals_max - totals_min) {
-      var options = [
-        <span><b>one year</b> renewal followed by another <b>one year</b> renewal</span>,
-        <span><b>one year</b> renewal followed by a <b>two year</b> renewal</span>,
-        <span><b>two year</b> renewal</span>,
-      ];
-      var cheapest_option = options.find(function (el, i) {
-        return totals_min == totals[i];
-      });
+      var cheapest_option;
+      if (totals[0] == totals_min) {
+        cheapest_option = (<span>
+          <b>one year</b> renewal followed by another <b>one year</b> renewal
+        </span>);
+      } else if (totals[1] == totals_min) {
+        cheapest_option = (<span>
+          <b>one year</b> renewal followed by a <b>two year</b> renewal
+        </span>);
+      } else if (totals[2] == totals_min) {
+        cheapest_option = <span><b>two year</b> renewal</span>;
+      }
 
       summary = (
         <p>
@@ -206,90 +213,96 @@ var App = React.createClass({
     } else if (!this.state.showDetails) {
       summary = (
         <p>
-          All your lease options work out to the same amount given {year} and {year + 1} rent adjustment rates. Moneywise, it doesn't matter which one you pick.
+          All your lease options work out to the same amount given {year} and {year + 1} rent adjustment rates. Moneywise, it doesn&apos;t matter which one you pick.
         </p>
       );
     }
 
     var details = (
-      <p><a href="/" onClick={this.showDetails}>Show me the details.</a></p>
+      <p><a href="/" onClick={this.showDetails.bind(this)}>Show me the details.</a></p>
     );
 
     if (this.state.showDetails) {
       details = (<div>
         <table>
           <caption>One year renewal followed by another one year renewal:</caption>
-          <tr>
-            <th scope="row">first year</th>
-            <td>
-              ${number_format(rent_1y * 12)}
-              <div className="monthly-rent">${number_format(rent_1y)} / mo</div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">next year</th>
-            <td>
-              ${number_format(rent_1y_1y * 12)}
-              <div className="monthly-rent">${number_format(rent_1y_1y)} / mo</div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">total</th>
-            <td className={totals_classes.hasOwnProperty(totals[0]) && totals_classes[totals[0]]}>
-              ${number_format(totals[0])}
-              <div className="monthly-rent">${number_format(totals[0] / 24)} / mo</div>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <th scope="row">first year</th>
+              <td>
+                ${number_format(rent_1y * 12)}
+                <div className="monthly-rent">${number_format(rent_1y)} / mo</div>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">next year</th>
+              <td>
+                ${number_format(rent_1y_1y * 12)}
+                <div className="monthly-rent">${number_format(rent_1y_1y)} / mo</div>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">total</th>
+              <td className={totals_classes.hasOwnProperty(totals[0]) && totals_classes[totals[0]]}>
+                ${number_format(totals[0])}
+                <div className="monthly-rent">${number_format(totals[0] / 24)} / mo</div>
+              </td>
+            </tr>
+          </tbody>
         </table>
 
         <table>
           <caption>One year followed by a two year:</caption>
-          <tr>
-            <th scope="row">first year</th>
-            <td>
-              ${number_format(rent_1y * 12)}
-              <div className="monthly-rent">${number_format(rent_1y)} / mo</div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">next year</th>
-            <td>
-              ${number_format(rent_1y_2y * 12)}
-              <div className="monthly-rent">${number_format(rent_1y_2y)} / mo</div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">total</th>
-            <td className={totals_classes.hasOwnProperty(totals[1]) && totals_classes[totals[1]]}>
-              ${number_format(totals[1])}
-              <div className="monthly-rent">${number_format(totals[1] / 24)} / mo</div>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <th scope="row">first year</th>
+              <td>
+                ${number_format(rent_1y * 12)}
+                <div className="monthly-rent">${number_format(rent_1y)} / mo</div>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">next year</th>
+              <td>
+                ${number_format(rent_1y_2y * 12)}
+                <div className="monthly-rent">${number_format(rent_1y_2y)} / mo</div>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">total</th>
+              <td className={totals_classes.hasOwnProperty(totals[1]) && totals_classes[totals[1]]}>
+                ${number_format(totals[1])}
+                <div className="monthly-rent">${number_format(totals[1] / 24)} / mo</div>
+              </td>
+            </tr>
+          </tbody>
         </table>
 
         <table>
           <caption>Two year renewal:</caption>
-          <tr>
-            <th scope="row">first year</th>
-            <td>
-              ${number_format(rent_2y * 12)}
-              <div className="monthly-rent">${number_format(rent_2y)} / mo</div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">next year</th>
-            <td>
-              ${number_format(rent_2y * 12)}
-              <div className="monthly-rent">${number_format(rent_2y)} / mo</div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">total</th>
-            <td className={totals_classes.hasOwnProperty(totals[2]) && totals_classes[totals[2]]}>
-              ${number_format(totals[2])}
-              <div className="monthly-rent">${number_format(totals[2] / 24)} / mo</div>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <th scope="row">first year</th>
+              <td>
+                ${number_format(rent_2y * 12)}
+                <div className="monthly-rent">${number_format(rent_2y)} / mo</div>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">next year</th>
+              <td>
+                ${number_format(rent_2y * 12)}
+                <div className="monthly-rent">${number_format(rent_2y)} / mo</div>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">total</th>
+              <td className={totals_classes.hasOwnProperty(totals[2]) && totals_classes[totals[2]]}>
+                ${number_format(totals[2])}
+                <div className="monthly-rent">${number_format(totals[2] / 24)} / mo</div>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>);
     }
@@ -312,6 +325,6 @@ var App = React.createClass({
       </div>
     );
   }
-});
+}
 
-React.render(<App />, document.getElementById('content'));
+ReactDOM.render(<App />, document.getElementById('content'));
